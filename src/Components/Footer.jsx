d267eb611c894630b96369ee98c8a0412b9bb28b
baalaji03 <rps123a@gmail.com>
2024-08-02 13:36:12 -0700

    completed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+    
+    <footer class="footer fixed-bottom bg-dark text-light p-3">
+   
+  </footer>
+
+    </>
+  );
+}
+
+export default Footer;
